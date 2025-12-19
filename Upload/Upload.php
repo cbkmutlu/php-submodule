6a6052194b1c58eab8c6e5bd8e36630f1da36868
cbkmutlu <cbkmutlu@gmail.com>
2025-12-19 03:22:15 +0300

    refactor(Upload): Introduce UploadAdapter and refactor Upload class

--- Upload/Upload.php
+++ Upload/Upload.php
@@ -7,66 +7,123 @@ namespace System\Upload;
 use System\Language\Language;
 use System\Exception\SystemException;
 
+interface UploadAdapter {
+   public function upload(array $file, string $path, string $name): bool;
+   public function unlink(string|array $files, string $path): bool;
+}
+
 class Upload {
-   private $file;
-   private $path;
-   private $allowed_types;
-   private $allowed_mimes;
-   private $max_width = 0;
-   private $min_width = 0;
-   private $max_height = 0;
-   private $min_height = 0;
-   private $max_size = 0;
-   private $min_size = 0;
-   private $error = [];
+   private array $allowed_types;
+   private array $allowed_mimes;
+   private int $max_width = 0;
+   private int $min_width = 0;
+   private int $max_height = 0;
+   private int $min_height = 0;
+   private int $max_size = 0;
+   private int $min_size = 0;
+   private array $error = [];
+   private string $path;
+   private string $dir = '';
+   private UploadAdapter $adapter;
 
    public function __construct(
       private Language $language
    ) {
-      $config = import_config('defines.upload');
-      $this->path = $config['path'];
+      $config              = import_config('defines.upload');
+      $this->adapter       = new $config['adapter']();
       $this->allowed_types = $config['allowed_types'];
       $this->allowed_mimes = $config['allowed_mimes'];
+      $this->path          = ROOT_DIR . $config['path'];
+      $this->checkPath();
    }
 
-   public function handle(array $file, ?string $filename = null): bool {
-      if (empty($file['tmp_name'])) {
-         $this->error['err_no_file'] = $this->language->system('upload.err_no_file');
-         return false;
-      }
-
-      $pathname = ROOT_DIR . $this->path;
-      $filename = $this->checkFileName($pathname, $filename ?? $file['name']);
-
-      $this->file = $file;
-      $this->checkTypes();
-      $this->checkMimes();
-      $this->checkDimension();
-      $this->checkSize();
+   public function setPath(string $path): self {
+      $this->path = ROOT_DIR . $path;
       $this->checkPath();
+      return $this;
+   }
 
-      if (!is_uploaded_file($file['tmp_name'])) {
-         throw new SystemException('File upload error');
+   public function getPath(): string {
+      return $this->path;
+   }
+
+   public function setDir(string $dir): self {
+      $this->dir = $dir;
+      $this->path .= '/' . $dir;
+      $this->checkPath();
+      return $this;
+   }
+
+   private function checkPath(): void {
+      if (!check_path($this->path)) {
+         throw new SystemException('Upload directory [' . $this->path . '] cannot be created');
       }
 
-      if (!move_uploaded_file($file['tmp_name'], $pathname . '/' . $filename)) {
-         throw new SystemException('File upload error');
+      if (!check_permission($this->path)) {
+         throw new SystemException('Upload directory [' . $this->path . '] is not writable');
+      }
+   }
+
+   public function handle(array $files): array {
+      $this->error = [];
+
+      $result = [];
+      if (!is_array($files['name'])) {
+         $files = [
+            'name'      => [$files['name']],
+            'tmp_name'  => [$files['tmp_name']],
+            'type'      => [$files['type']],
+            'error'     => [$files['error']],
+            'size'      => [$files['size']],
+         ];
       }
 
-      return empty($this->error);
+      foreach ($files['name'] as $i => $name) {
+         $file = [
+            'name'     => $files['name'][$i],
+            'tmp_name' => $files['tmp_name'][$i],
+            'type'     => $files['type'][$i],
+            'error'    => $files['error'][$i],
+            'size'     => $files['size'][$i],
+         ];
+
+         if (empty($file['tmp_name'])) {
+            $this->error['err_no_file'] = $this->language->system('upload.err_no_file');
+            continue;
+         }
+
+         $this->checkTypes($file);
+         $this->checkMimes($file);
+         $this->checkDimension($file);
+         $this->checkSize($file);
+
+         if (!empty($this->error)) {
+            continue;
+         }
+
+         $name = bin2hex(random_bytes(16)) . '.' . pathinfo($file['name'], PATHINFO_EXTENSION);
+         $this->adapter->upload($file, $this->path, $name);
+         $result[] = $this->dir . '/' . $name;
+      }
+
+      if (empty($result) && !empty($this->error)) {
+         throw new SystemException(json_encode($this->error, JSON_UNESCAPED_UNICODE), 400);
+      }
+
+      return $result;
+   }
+
+   public function unlink(string|array $files): bool {
+      return $this->adapter->unlink($files, $this->path);
    }
 
    public function error(): array {
       return $this->error;
    }
 
-   public function setPath(string $path): self {
-      $this->path = $path;
+   public function setAdapter(UploadAdapter $adapter): self {
+      $this->adapter = $adapter;
       return $this;
-   }
-
-   public function getPath(): string {
-      return $this->path;
    }
 
    public function setAllowedTypes(array $types): self {
@@ -141,30 +198,15 @@ class Upload {
       return $this->min_size;
    }
 
-   private function checkFileName(string $directory, string $filename): string {
-      $pathinfo = pathinfo($filename);
-      $basename = $pathinfo['filename'];
-      $extension = isset($pathinfo['extension']) ? '.' . $pathinfo['extension'] : '';
-      $i = 1;
-      $filename = $basename . $extension;
-
-      while (file_exists($directory . '/' . $filename)) {
-         $filename = $basename . '(' . $i . ')' . $extension;
-         $i++;
-      }
-
-      return $filename;
-   }
-
-   private function checkDimension(): void {
-      if ($this->max_height > 0 || $this->min_height > 0 || $this->max_width > 0 || $this->min_width > 0) {
-         $mime = mime_content_type($this->file['tmp_name']);
+   private function checkDimension(array $file): void {
+      if ($this->max_height || $this->min_height || $this->max_width || $this->min_width) {
+         $mime = mime_content_type($file['tmp_name']);
          if (!str_starts_with($mime, 'image/')) {
             return;
          }
       }
 
-      $size = getimagesize($this->file['tmp_name']);
+      $size = getimagesize($file['tmp_name']);
       if ($size === false) {
          $this->error['err_file_size'] = $this->language->system('upload.err_file_size');
          return;
@@ -172,39 +214,39 @@ class Upload {
 
       [$width, $height] = $size;
 
-      if (($this->max_width > 0 && $width > $this->max_width) || ($this->max_height > 0 && $height > $this->max_height)) {
+      if (($this->max_width && $width > $this->max_width) || ($this->max_height && $height > $this->max_height)) {
          $this->error['err_max_dimension'] = $this->language->system('upload.err_max_dimension', [$this->max_width, $this->max_height]);
       }
 
-      if (($this->min_width > 0 && $width < $this->min_width) || ($this->min_height > 0 && $height < $this->min_height)) {
+      if (($this->min_width && $width < $this->min_width) || ($this->min_height && $height < $this->min_height)) {
          $this->error['err_min_dimension'] = $this->language->system('upload.err_min_dimension', [$this->min_width, $this->min_height]);
       }
    }
 
-   private function checkSize(): void {
-      if ($this->max_size > 0 && $this->file['size'] > $this->max_size * 1024) {
+   private function checkSize(array $file): void {
+      if ($this->max_size && $file['size'] > $this->max_size * 1024) {
          $this->error['err_max_size'] = $this->language->system('upload.err_max_size', [$this->max_size]);
       }
 
-      if ($this->min_size > 0 && $this->file['size'] < $this->min_size * 1024) {
+      if ($this->min_size && $file['size'] < $this->min_size * 1024) {
          $this->error['err_min_size'] = $this->language->system('upload.err_min_size', [$this->min_size]);
       }
    }
 
-   private function checkTypes(): void {
-      $type = pathinfo($this->file['name'], PATHINFO_EXTENSION);
+   private function checkTypes(array $file): void {
+      $type = pathinfo($file['name'], PATHINFO_EXTENSION);
       if (!in_array($type, $this->allowed_types)) {
          $this->error['err_file_type'] = $this->language->system('upload.err_file_type');
       }
    }
 
-   private function checkMimes(): void {
-      $file = mime_content_type($this->file['tmp_name']);
+   private function checkMimes(array $file): void {
+      $mime = mime_content_type($file['tmp_name']);
       $matched = false;
 
-      foreach ($this->allowed_mimes as $mime) {
-         $pattern = '#^' . str_replace('\*', '.*', preg_quote($mime, '#')) . '$#i';
-         if (preg_match($pattern, $file)) {
+      foreach ($this->allowed_mimes as $pattern) {
+         $regex = '#^' . str_replace('\*', '.*', preg_quote($pattern, '#')) . '$#i';
+         if (preg_match($regex, $mime)) {
             $matched = true;
             break;
          }
@@ -212,17 +254,6 @@ class Upload {
 
       if (!$matched) {
          $this->error['err_file_type'] = $this->language->system('upload.err_file_type');
-      }
-   }
-
-   private function checkPath(): void {
-      $pathname = ROOT_DIR . $this->path;
-      if (!check_path($pathname)) {
-         throw new SystemException('File upload directory [' . $pathname . '] is invalid');
-      }
-
-      if (!check_permission($pathname)) {
-         throw new SystemException('File upload directory [' . $pathname . '] is not writable');
       }
    }
 }
